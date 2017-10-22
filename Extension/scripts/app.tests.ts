ed8ddb930880dfa56df49f415ba1a8cab0c53beb
Stanislaw Swierc <stansw@microsoft.com>
2017-10-22 06:40:02 -0700

    Save small improvements to the extension project

---
+++ Extension/scripts/app.tests.ts
@@ -0,0 +1,8 @@
+import * as App from "./app"
+import { expect } from "chai";
+
+describe("Message", () => {
+    it("should return all work item ids", () => {
+        expect("1").to.be.equal("2");
+    });
+});
